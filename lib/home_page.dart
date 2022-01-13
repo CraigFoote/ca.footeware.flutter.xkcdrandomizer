71bb7e7d6d66a467e18639dded4baed8cd3a7af9
CraigFoote <CraigFoote@gmail.com>
2022-01-13 08:58:25 -0500

    sharing

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,16 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart';
+import 'package:http/io_client.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'comic_page.dart';
 import 'custom_theme.dart';
 import 'info_page.dart';
 
@@ -16,14 +25,20 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   bool switchValue = false;
-  final PageController _controller = PageController(
-    keepPage: false,
-  );
+  late String _url;
+  late bool _haveUrl;
+  late bool _haveRequest;
 
   @override
   void initState() {
     super.initState();
     _loadPrefs();
+    _haveUrl = false;
+    _haveRequest = false;
+  }
+
+  Future<PermissionStatus> get permission async {
+    return await Permission.storage.request();
   }
 
   void _loadPrefs() async {
@@ -38,98 +53,188 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'XKCD Randomizer',
-        ),
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-              tooltip: 'Preferences',
-            );
-          },
-        ),
-      ),
-      drawer: Drawer(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: ListView(
-            children: [
-              Row(
-                children: [
-                  const Text('Dark Theme'),
-                  Switch(
-                    value: switchValue,
-                    onChanged: (value) async {
-                      final prefs = await SharedPreferences.getInstance();
-                      setState(() {
-                        switchValue = value;
-                        prefs.setBool('darkTheme', switchValue);
-                        widget.themeCallback(value
-                            ? CustomTheme.darkTheme
-                            : CustomTheme.lightTheme);
-                      });
-                    },
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: GestureDetector(
-                      child: Builder(
-                        builder: (_) => const Icon(
-                          Icons.info,
-                        ),
-                      ),
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (_) {
-                              return const InfoPage(
-                                title: 'Info',
-                              );
-                            },
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ],
+      appBar: buildAppbar(),
+      drawer: buildDrawer(),
+      body: buildBody(),
+    );
+  }
+
+  void _getComic() async {
+    setState(() => _haveRequest = true);
+    var url = Uri(scheme: 'https', host: 'random-xkcd-img.herokuapp.com');
+    final client = HttpClient();
+    client.badCertificateCallback =
+        (X509Certificate cert, String host, int port) => true;
+    final http = IOClient(client);
+    await http.get(url).then(
+      (response) async {
+        if (response.statusCode == 200) {
+          Map<String, dynamic> result = json.decode(response.body);
+          _url = result['url'];
+          setState(() {
+            _haveUrl = true;
+            _haveRequest = false;
+          });
+        }
+      },
+    );
+  }
+
+  Future<void> share(String url) async {
+    if (Platform.isAndroid) {
+      if (await permission.isGranted) {
+        var uri = Uri.parse(url);
+        String path = uri.path.toString();
+        String filename = path.substring(path.lastIndexOf('/') + 1);
+        var response = await get(uri);
+        var directory = await getTemporaryDirectory();
+        File imgFile = File('${directory.path}/$filename');
+        var bodyBytes = response.bodyBytes;
+        imgFile.writeAsBytes(bodyBytes);
+        final box = context.findRenderObject() as RenderBox;
+        Share.shareFiles([imgFile.path],
+            subject: 'XKCD comic',
+            sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+      }
+    } else {
+      Clipboard.setData(ClipboardData(text: url));
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'URL copied to clipboard.',
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
+      );
+    }
+  }
+
+  buildAppbar() {
+    return AppBar(
+      title: const Text(
+        'XKCD Randomizer',
       ),
-      body: PageView.builder(
-        controller: _controller,
-        itemCount: 1000,
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                Text(
-                  'Swipe to see a random XKCD comic.',
-                  style: TextStyle(
-                    fontSize: 18.0,
-                  ),
-                ),
-                Icon(
-                  Icons.swipe,
+      leading: Builder(
+        builder: (BuildContext context) {
+          return IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            tooltip: 'Preferences',
+          );
+        },
+      ),
+      actions: [
+        OutlinedButton.icon(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          ),
+          label: const Text(
+            'Next',
+          ),
+          onPressed: () => _getComic(),
+          icon: const Icon(
+            Icons.arrow_forward_rounded,
+          ),
+        ),
+        OutlinedButton.icon(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          ),
+          label: const Text(
+            'Share',
+          ),
+          onPressed: () => _haveUrl ? share(_url) : null,
+          icon: const Icon(
+            Icons.share,
+          ),
+        )
+      ],
+    );
+  }
+
+  buildDrawer() {
+    return Drawer(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text('Dark Theme'),
+                Switch(
+                  value: switchValue,
+                  onChanged: (value) async {
+                    final prefs = await SharedPreferences.getInstance();
+                    setState(() {
+                      switchValue = value;
+                      prefs.setBool('darkTheme', switchValue);
+                      widget.themeCallback(value
+                          ? CustomTheme.darkTheme
+                          : CustomTheme.lightTheme);
+                    });
+                  },
                 ),
               ],
-            );
-          } else {
-            return const ComicPage();
-          }
-        },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: GestureDetector(
+                    child: Builder(
+                      builder: (_) => const Icon(
+                        Icons.info,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (_) {
+                            return const InfoPage(
+                              title: 'Info',
+                            );
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  buildBody() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Center(
+        child: _haveRequest
+            ? const CircularProgressIndicator()
+            : _haveUrl
+                ? PhotoView(
+                    imageProvider: NetworkImage(
+                      _url,
+                    ),
+                    backgroundDecoration: const BoxDecoration(
+                      color: Colors.transparent,
+                    ),
+                  )
+                : const Text(
+                    'XKCD',
+                    style: TextStyle(
+                      fontSize: 32,
+                    ),
+                  ),
       ),
     );
   }
