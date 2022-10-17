207400d337ebe0d50c9da5f6ed76521aaab9b490
CraigFoote <CraigFoote@gmail.com>
2022-10-17 17:48:22 -0400

    update deps

--- lib/info_page.dart
+++ lib/info_page.dart
@@ -9,35 +9,36 @@ class InfoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            title,
-          ),
+      appBar: AppBar(
+        title: Text(
+          title,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'images/programmer.jpg',
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Linkify(
-                  text: 'Another fine mess by http://Footeware.ca',
-                  onOpen: _openUrl,
-                ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'images/programmer.jpg',
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Linkify(
+                text: 'Another fine mess by http://Footeware.ca',
+                onOpen: _openUrl,
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 
   void _openUrl(link) async {
-    if (await canLaunch(link.url)) {
-      await launch(
-        link.url,
-        forceWebView: false,
+    if (await canLaunchUrl(Uri.parse(link.url))) {
+      await launchUrl(
+        Uri.parse(link.url),
+        mode: LaunchMode.externalApplication,
       );
     } else {
       throw 'Could not launch $link';
